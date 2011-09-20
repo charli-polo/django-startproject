0f8c29b6c8001cdfacefee14c6062d3f1267c1a3
Charlie <charles-antoine.idrac_git@m4x.org>
2011-09-21 01:08:22 +0200

    complete use of dotcloud config

---
+++ django_startproject/project_template/myproject/conf/dotcloud/settings.py
@@ -0,0 +1,28 @@
+from PROJECT.conf.settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('You', 'your@email'),
+)
+MANAGERS = ADMINS
+
+STATIC_ROOT = '/home/dotcloud/data/static/'
+MEDIA_ROOT = '/home/dotcloud/data/media/'
+VAR_ROOT = '/home/dotcloud/data/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(VAR_ROOT, 'dev.db'),
+    }
+}
+
+ROOT_URLCONF = '%s.conf.dotcloud.urls' % PROJECT_MODULE_NAME
+
+
+INSTALLED_APPS += (
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+)
