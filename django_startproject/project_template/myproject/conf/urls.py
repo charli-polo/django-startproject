03fd8490c23032f123fbfe28071dd7e088323b76
Charlie <charles-antoine.idrac_git@m4x.org>
2011-09-21 01:18:58 +0200

    complete use of dotcloud config

--- django_startproject/project_template/myproject/conf/urls.py
+++ django_startproject/project_template/myproject/conf/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, url, include
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # project urls here
+    (r'^$', TemplateView.as_view(template_name="fresh_start/start.html")),
 )
